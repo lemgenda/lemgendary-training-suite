4de175e140bf21ffa4c517e1d3386014ebaec7e4
kuzn3tsov <lem.treursic@gmail.com>
2026-04-08 03:47:11 +0200

    chore: add IDE structural requirement stubs

---
+++ data/yolo_config_gen.py
@@ -0,0 +1,11 @@
+import os
+import yaml
+
+def generate_yolo_yaml(config, model_key, unified_models, unified_data):
+    """
+    Placeholder generator for YOLO multi-task configs.
+    Dynamically generates Ultralytics .yaml files based on unified data registries.
+    """
+    # 2026: This function is triggered natively by train.py when args.model contains 'yolo'
+    # Resolves local import references for IDE validation.
+    return "data/yolo_temp_config.yaml"
